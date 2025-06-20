cd6ce34aaff83a22ec2980c92e3586ad8ac1310f
Aditya Padale <adityapadale25@gmail.com>
2025-06-21 00:16:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: "client", // Set root to client so index.html is at dist root
   plugins: [react()],
-  root: "client",
   build: {
-    outDir: "dist",
+    outDir: "../dist", // Output to project root dist
     emptyOutDir: true,
   },
   resolve: {
