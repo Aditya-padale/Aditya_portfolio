49bc3263cca1907181fdee5840029e5c62039d78
Aditya Padale <adityapadale25@gmail.com>
2025-06-21 00:23:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   root: "client", // Set root to client so index.html is at dist root
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "img",
+          dest: ""
+        }
+      ]
+    })
+  ],
   build: {
     outDir: "../dist", // Output to project root dist
     emptyOutDir: true,
